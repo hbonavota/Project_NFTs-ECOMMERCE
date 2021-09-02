a5348d729836e48edfde70118c928e8b1346539c
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2021-09-02 20:00:30 -0300

    routes and post

--- backend/src/controllers/products/products.js
+++ backend/src/controllers/products/products.js
@@ -1,15 +1,16 @@
 const Product = require('../../models/Product')
+const axios = require('axios').default
 
 async function createProduct (req, res)  {
     const {
-        name, description, price, image, tokenId, categories, artist, specs, reviews, collection, } = req.body
+        name, description, price, image, tokenId, categories, artist, specs, reviews, collection } = req.body
     const newProduct  = new Product (
         {
         name, 
         description,
         price,
         image,
-        tokenId,
+        tokenId, //--> lo traigo desde CDI-IPFS guardado previamente en Mongo
         categories,
         artist,
         specs,
@@ -22,8 +23,39 @@ async function createProduct (req, res)  {
 }
 
 async function getProducts (req, res)  {
-    console.log(req.body)
-    res.json(' get products')
+    try {
+        const nfts = await axios.get('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50')
+        const nft = nfts.data.assets; 
+        let dataAssets = []
+ 
+        for (let data of nft) {   
+           const assets = {
+            name: data.name,
+            image: data.image_url,
+            id: data.id,
+            // short_description:data.collection,
+            sales: data.num_sales,
+            token_id: data.token_id,
+            description: data.collection.description,
+            address: data.address,
+            featured: data.featured,
+            featured_image_url: data.featured_image_url,
+            twitter_username: data.twitter_username,
+            instagram_username: data.instagram_username,
+            // owner: data.user.username,
+            addressOwner: data.owner.address,
+         //    creator: data.creator.user.username,
+         //    addressCreator: data.creator.address,
+           }
+           dataAssets.push(assets)
+         }
+           
+           res.send(dataAssets)
+
+     }
+     catch(err){
+         console.log(err)
+     }
 }
 
 async function getProductById (req, res)  {
--- backend/src/models/Prices.js
+++ backend/src/models/Prices.js
@@ -1,6 +1,5 @@
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema, mongoose } = require('mongoose');
 
 const PriceSchema = new Schema({
 	currency: String,
