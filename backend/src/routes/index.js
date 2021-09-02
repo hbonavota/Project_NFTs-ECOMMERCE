17d89c005f8c0e90a2dc43d070842b3d33575589
Sofia <sofiabeleninchausti@gmail.com>
2021-09-02 12:25:57 -0300

    get-NFTs

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
-const router = require('express').Router();
+const axios = require('axios')
+const router = Router();
 const auth = require('../controllers/user/auth.js');
+const {getAssets}=require ("./openSeaNft")
 
 function isLoggedIn(req,res,next){
     req.user? next() : res.sendStatus(401)
@@ -13,6 +15,46 @@ const { connectWallet } = require('../controllers/crypto/connectWallet.routes')
 
 // Routes      
 router.get('/connect', connectWallet)
+
+// router.use("/nft",getAssets)
+router.get("/nft",async (req,res,next)=>{ 
+    try {
+        const nfts = await axios.get('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50')
+        const nft = nfts.data.assets; 
+        let dataAssets = []
+ 
+        for (let data of nft) {   
+           const assets = {
+            name: data.name,
+            image: data.image_url,
+            id: data.id,
+            // short_description:data.collection,
+            sales: data.num_sales,
+            token_id: data.token_id,
+            description: data.collection.description,
+            address: data.address,
+            featured: data.featured,
+            featured_image_url: data.featured_image_url,
+            twitter_username: data.twitter_username,
+            instagram_username: data.instagram_username,
+            owner: data.user.username,
+            addressOwner: data.owner.address,
+         //    creator: data.creator.user.username,
+         //    addressCreator: data.creator.address,
+           }
+           dataAssets.push(assets)
+         }
+           
+           res.send(dataAssets)
+     }
+     catch(err){
+         console.log(err)
+     }
+
+     
+ 
+})
+
 
 /* router.get('/auth/google',
 passport.authenticate('google'), {scope:['email','profile']} ) 
