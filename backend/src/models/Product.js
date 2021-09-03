2a19c61e70a604fe00b1a9c659a702eec5de352a
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2021-09-02 22:39:52 -0300

    save POST and GETS

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -4,13 +4,10 @@ const Schema = mongoose.Schema;
 // const findOrCreate = require('mongoose-findorcreate');
 
 const ProductsSchema = new Schema({
-	name: {
-		type: String,
-		unique: true,
-	},
+	name: { type: String, require: true, index:true, unique:false, sparse:true},
 	description: String,
 	price: {currency: String, value: Number},
-	image: String,
+	image: [],
 	tokenId: String,
 	// categories: [
 	// 	{
