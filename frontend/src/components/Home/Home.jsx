e01e1cca5fbb416c0dc88181f935d2749194ef89
Agus Corbalan <tinicorb@gmail.com>
2021-09-03 07:28:52 -0300

    paginado/agus

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,48 +1,69 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getNFTs } from '../../actions/getNFTs.js';
-import NavBar from '../NavBar/NavBar';
-import { loading } from '../../actions/loading.js';
-import style from '../Home/Home.module.css'
-
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getNFTs, pageUno } from "../../actions/getNFTs.js";
+import NavBar from "../NavBar/NavBar";
+import { loading } from "../../actions/loading.js";
+import style from "../Home/Home.module.css";
+import Paginated from "../Paginado/Paginated";
 export default function Home() {
-    const StateFilteredNFTs = useSelector(state => state.filtered)
-    const stateAllNFTs = useSelector(state => state.allNFTs)
-    const stateLoading = useSelector(state => state.loading)
+  const StateFilteredNFTs = useSelector((state) => state.filtered);
+  const stateAllNFTs = useSelector((state) => state.allNFTs);
+  const stateLoading = useSelector((state) => state.loading);
+  const pages = useSelector((state) => state.page);
+  const [PerPage, setGamePerPage] = useState(9);
+  const lastNft = pages * PerPage;
+  const firstNft = lastNft - PerPage;
+  const currentNft = stateAllNFTs.slice(firstNft, lastNft);
 
-    console.log(stateAllNFTs,"nftssssss")
+  const paged = (pageNumber) => {
+    dispatch(pageUno(pageNumber));
+  };
 
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getNFTs())
-        //dispatch(loading(true))
-        return () => {
-            dispatch(getNFTs())
-        }
-    }, [dispatch]);
+  console.log(stateAllNFTs, "nftssssss");
 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getNFTs());
+    //dispatch(loading(true))
+    return () => {
+      dispatch(getNFTs());
+    };
+  }, [dispatch]);
 
-    return (
-        <div>
-            <div className= /* {stateLoading ? style.back2 : style.back1} */{style.back1}>
-                <div className={style.container}>
-                    <h1> Welcome to NFTs-ECOMMERCE</h1>
-                    {stateAllNFTs?
-                    stateAllNFTs.map(n=><div key={n.id}>
-                        <h2>{n.name}</h2>
-                        <img src={n.image}/>
-                        <p>{n.description}</p>
-                        {/* <div>
+  return (
+    <div>
+      <div
+        className=/* {stateLoading ? style.back2 : style.back1} */ {style.back1}
+      >
+        <div className={style.container}>
+          <h1> Welcome to NFTs-ECOMMERCE</h1>
+          {currentNft ? (
+            currentNft.map((n) => (
+              <div key={n.id}>
+                <h2>{n.name}</h2>
+                <img src={n.image} />
+                <p>{n.description}</p>
+                {/* <div>
                         <p>{n.owner}</p>
                         </div> */}
-                         </div>)
-                :<p>loading...</p>}
-                   
-            
-      
-                </div>
-            </div>
+              </div>
+            ))
+          ) : (
+            <p>loading...</p>
+          )}
+          <div>
+            <Paginated
+              PerPage={PerPage}
+              stateAllNFTs={stateAllNFTs.length}
+              paged={paged}
+            />
+            {currentNft?.map((e, index) => {
+              // le pregunto si hay AllGames, (tiene el estado global con todos los juegos) y desp por cada juego le paso un card con cada prop que quiero renderizar
+              return <p key={index} nft={e} />;
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,28 +1,35 @@
-import { GET_NTFs,  SET_LOADING } from '../actions/constants'
+import { GET_NTFs, SET_LOADING } from "../actions/constants";
 
 const initialState = {
-    allNFTs: [], // state of all NFTS from API openSea
-    filtered: [], // contains array for make the filters
-    loading: true //  boolean for show a image when is loading. Set first : true
+  allNFTs: [], // state of all NFTS from API openSea
+  filtered: [], // contains array for make the filters
+  loading: true, //  boolean for show a image when is loading. Set first : true
+  page: 1,
 };
 
 function rootReducer(state = initialState, action) {
-    switch (action.type) {
-        case GET_NTFs:
-            return {
-                ...state,
-                allNFTs: action.payload,
-                filtered: action.payload,
-                loading: false
-            };
-        case SET_LOADING:
-            return {
-                ...state,
-                loading: action.payload
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case GET_NTFs:
+      return {
+        ...state,
+        page: 1,
+        allNFTs: action.payload,
+        filtered: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case "PAGE":
+      return {
+        ...state,
+        page: action.payload,
+      };
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
