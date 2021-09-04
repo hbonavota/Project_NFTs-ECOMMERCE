08f44086e42308c14ac9be4d1beb7cdd0457d415
Agus Corbalan <tinicorb@gmail.com>
2021-09-04 11:58:55 -0300

    Delete Route

--- backend/src/controllers/products/products.js
+++ backend/src/controllers/products/products.js
@@ -1,150 +1,160 @@
-const Product = require('../../models/Product')
-const Web3 = require('web3')
+const Product = require("../../models/Product");
+const Web3 = require("web3");
 const web3 = new Web3();
-const axios = require('axios').default
+const axios = require("axios").default;
 const { API_KEY_OPENSEA } = process.env;
 
-async function createProduct (req, res)  {
-    try {
-        const { name, description, price, image, categories, artist, address, reviews, collection, currency} = req.body
-        const randomString = web3.utils.sha3(Math.random(0, 1000000).toString(16) + web3.utils.randomHex(32))
-        const sevenHundred = web3.eth.accounts.wallet.create(1, randomString)
+async function createProduct(req, res) {
+  try {
+    const {
+      name,
+      description,
+      price,
+      image,
+      categories,
+      artist,
+      address,
+      reviews,
+      collection,
+      currency,
+    } = req.body;
+    const randomString = web3.utils.sha3(
+      Math.random(0, 1000000).toString(16) + web3.utils.randomHex(32)
+    );
+    const sevenHundred = web3.eth.accounts.wallet.create(1, randomString);
 
-        let tokenId = sevenHundred[0].address;
-        const newProduct  = new Product (
-            {
-            name, 
-            description,
-            price,
-            currency,
-            image, 
-            tokenId,
-            categories,
-            artist,
-            address,
-            reviews,
-            collection,
-            }
-        )
+    let tokenId = sevenHundred[0].address;
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      currency,
+      image,
+      tokenId,
+      categories,
+      artist,
+      address,
+      reviews,
+      collection,
+    });
 
-        const productSaved = await newProduct.save()
-        res.status(201).json(productSaved)
-
-    } catch(error) {
-    console.log(error)
-    res.json(error)
+    const productSaved = await newProduct.save();
+    res.status(201).json(productSaved);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
 }
-}
 
-async function getProductsApi ()  {
-    try{
-            const nfts = await axios.get('https://api.coinranking.com/v2/nfts?&limit=100');
-            const nft = nfts.data.data.nfts;
-            let dataAssets = [];    
-            for (let n of nft) {
-                    const assets = { 
-                        name: n.name,
-                        image: n.image,
-                        id: n.id,
-                        price:n.price,
-                        price_dolar:n.priceInDollar,     
-                        tokenId:n.tokenId
-              };
-            dataAssets.push(assets);
-            }
-            return dataAssets;
-      
+async function getProductsApi() {
+  try {
+    const nfts = await axios.get(
+      "https://api.coinranking.com/v2/nfts?&limit=100"
+    );
+    const nft = nfts.data.data.nfts;
+    let dataAssets = [];
+    for (let n of nft) {
+      const assets = {
+        name: n.name,
+        image: n.image,
+        id: n.id,
+        price: n.price,
+        price_dolar: n.priceInDollar,
+        tokenId: n.tokenId,
+      };
+      dataAssets.push(assets);
     }
-    catch(error){
-        next('Error')
-    }
+    return dataAssets;
+  } catch (error) {
+    next("Error");
+  }
 }
 
-  
-
-async function getProductsDb ()  {
-    try {
-           const products = await Product.find()
-           return products;
-     }
-     catch(err){
-         console.log(err)
-     }
+async function getProductsDb() {
+  try {
+    const products = await Product.find();
+    return products;
+  } catch (err) {
+    console.log(err);
+  }
 }
-let getAll = async() => {
-    try {
-        const nftApi = await getProductsApi();
-        const nftDB = await getProductsDb();
-        const nftTotal = nftApi.concat(nftDB);
-        return nftTotal;
-    } catch (error) {
-        console.log(error)
-    }
-}
+let getAll = async () => {
+  try {
+    const nftApi = await getProductsApi();
+    const nftDB = await getProductsDb();
+    const nftTotal = nftApi.concat(nftDB);
+    return nftTotal;
+  } catch (error) {
+    console.log(error);
+  }
+};
 //FUNCION QUE HACE EL GET
-let getNFTs = async(_req, res) => {
-    try {
-        let nft = await getAll();
-        return res.json(nft);
-    } catch (error) {
-        console.log(error);
-    }
-}
+let getNFTs = async (_req, res) => {
+  try {
+    let nft = await getAll();
+    return res.json(nft);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-async function getProductById (req, res)  {
-   
-try {
+async function getProductById(req, res) {
+  try {
     const { id } = req.params;
 
-    const product = await Product.findById(id)
+    const product = await Product.findById(id);
 
-    const nfts = await axios.get(`https://api.opensea.io/api/v1/asset/${product.address}/${product.tokenId}/`)
+    const nfts = await axios.get(
+      `https://api.opensea.io/api/v1/asset/${product.address}/${product.tokenId}/`
+    );
     const nft = nfts.data;
-    
- 
-} catch(error) {
-    
-    return res.json(error)
+  } catch (error) {
+    return res.json(error);
+  }
 }
 
+async function searchProduct(req, res, next) {
+  var name = req.query.query;
+  try {
+    let nft = await getAll();
+    const result = nft.filter((n) => {
+      if (n.name && n.name.toLowerCase().includes(name.toLocaleLowerCase())) {
+        return n;
+      }
+    });
+    console.log(result);
+    return res.status(200).send(result);
+  } catch (error) {
+    next("error");
+  }
 }
 
-async function searchProduct(req, res,next) {
-    var name= req.query.query  
-    try{
-        let nft = await getAll();
-        const result=nft.filter(n=>{
-            if(n.name && n.name.toLowerCase().includes(name.toLocaleLowerCase())){
-                return n 
-            }
-        })
-        console.log(result)
-        return res.status(200).send(result)        
-    }    
-    catch(error){
-        next("error")
+async function updateProductById(req, res) {}
+
+async function deleteProductById(req, res) {
+  console.log("acaaaaa");
+  const id = req.params.id;
+  console.log("id desde backend", id);
+  try {
+    const nftDb = await Product.findByIdAndDelete({ _id: id });
+    console.log(nftDb, "ACAAAAAAA");
+    if (!nftDb) {
+      res.send("No se puede eliminar");
+    } else {
+      res.json("Eliminado");
     }
-     
+  } catch (error) {
+    next("error");
+  }
 }
-
-async function updateProductById (req, res)  {
-   
-    
-}
-
-async function deleteProductById (req, res)  {
-    
-}
-
-
 
 module.exports = {
-    createProduct,
-    getProductsApi,
-    getProductsDb,
-    getProductById,
-    updateProductById,
-    deleteProductById,
-    searchProduct,
-    getNFTs
-}
+  createProduct,
+  getProductsApi,
+  getProductsDb,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+  searchProduct,
+  getNFTs,
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,30 +1,39 @@
-const { Router } = require('express');
-const axios = require('axios')
+const { Router } = require("express");
+const axios = require("axios");
 const router = Router();
-const auth = require('../controllers/user/auth.js');
-const passport = require('passport');
-const {isLoggedIn} = require('../controllers/user/isLoggedIn')
-const {protected} = require('../controllers/apiGoogle/protected')
+const auth = require("../controllers/user/auth.js");
+const passport = require("passport");
+const { isLoggedIn } = require("../controllers/user/isLoggedIn");
+const { protected } = require("../controllers/apiGoogle/protected");
 
+const {
+  connectWallet,
+} = require("../controllers/payments/crypto/connectWallet.routes");
+const {
+  searchProduct,
+  createProduct,
+  getProductsApi,
+  getProductsDb,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+  getNFTs,
+} = require("../controllers/products/products");
 
-
-const { connectWallet } = require('../controllers/payments/crypto/connectWallet.routes');
-const { searchProduct,createProduct, getProductsApi, getProductsDb, getProductById, updateProductById, deleteProductById, getNFTs } = require('../controllers/products/products');
-
-// Routes      
+// Routes
 //router.get('/connect', connectWallet)
-router.get('/search',searchProduct);
-router.get('/connect', connectWallet);
-router.get('/nfts', getNFTs);
+router.get("/search", searchProduct);
+router.get("/connect", connectWallet);
+router.get("/nfts", getNFTs);
 // router.get('/nft', getProductsDb)
-router.get('/nft/:id', getProductById);
-router.post('/nft', createProduct);
+router.get("/nft/:id", getProductById);
+router.post("/nft", createProduct);
 
 // router.put('/nft', updateProductById)
-// router.delete('/nft', deleteProductById)
+router.delete("/delete/:id", deleteProductById);
 // router.use('/auth/google',isAuthenticated)
 // router.use('/google/callback',googleCallback)
 // router.use('/auth/failure', authFailure)
-router.use('/protected', isLoggedIn, protected);
+router.use("/protected", isLoggedIn, protected);
 
-module.exports = router 
+module.exports = router;
