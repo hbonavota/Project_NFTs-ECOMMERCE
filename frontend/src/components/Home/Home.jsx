78c3eda19208a88fea9d3e739e4db5e239d0c8b4
Agustina Corbalan <tinicorb@gmail.com>
2021-09-05 17:59:21 -0300

    SortByPrice

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getNFTs, pageUno } from "../../actions/getNFTs.js";
 import NavBar from "../NavBar/NavBar";
+import Cards from "../../components/card/card";
 import { loading } from "../../actions/loading.js";
 import style from "../Home/Home.module.css";
 import Paginated from "../Paginado/Paginated";
 import Search from "../Search/Search";
-import { Link } from 'react-router-dom';
-import sortByAbc from '../../actions/sortByAbc'
-
-
+import { Link } from "react-router-dom";
+import sortByAbc from "../../actions/sortByAbc";
+import { sortByPrice } from "../../actions/sortByPrice";
 
 export default function Home() {
   const StateFilteredNFTs = useSelector((state) => state.filtered);
@@ -21,20 +21,19 @@ export default function Home() {
   // const firstNft = lastNft - PerPage;
   // const currentNft = stateAllNFTs.slice(firstNft, lastNft);
 
-const [currentPage,setCurrentPage]=useState(1);//empieza por pagina 1
-const [PerPage, setNftPerPage]=useState([9]);// cuantos perros por pagina
-const lastNft=currentPage*PerPage;//ultimo perro primer caso 1*9
-const firstNft=lastNft-PerPage;//primer perro 9-9=0
-const currentNft=stateAllNFTs.slice(firstNft,lastNft);//aca hago el corte de mi arreglo de dogs
-const paginate=(pageNumber)=> setCurrentPage(pageNumber)
+  const [currentPage, setCurrentPage] = useState(1); //empieza por pagina 1
+  const [PerPage, setNftPerPage] = useState([9]); // cuantos perros por pagina
+  const lastNft = currentPage * PerPage; //ultimo perro primer caso 1*9
+  const firstNft = lastNft - PerPage; //primer perro 9-9=0
+  const currentNft = stateAllNFTs.slice(firstNft, lastNft); //aca hago el corte de mi arreglo de dogs
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  // const paginate=(pageNumber)=> setCurrentPage(pageNumber)
+  //   const paged = (pageNumber) => {
+  //     dispatch(pageUno(pageNumber));
+  //   };
 
-// const paginate=(pageNumber)=> setCurrentPage(pageNumber)
-//   const paged = (pageNumber) => {
-//     dispatch(pageUno(pageNumber));
-//   };
-
-//   console.log(stateAllNFTs, "nftssssss");
+  //   console.log(stateAllNFTs, "nftssssss");
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -45,11 +44,15 @@ const paginate=(pageNumber)=> setCurrentPage(pageNumber)
     };
   }, [dispatch]);
 
-  const filterAscDesc=(e)=>{
-    e.preventDefault()
-    dispatch(sortByAbc(e.target.value))
-  }
+  const filterAscDesc = (e) => {
+    e.preventDefault();
+    dispatch(sortByAbc(e.target.value));
+  };
 
+  const filterByPrice = (e) => {
+    e.preventDefault();
+    dispatch(sortByPrice(e.target.value));
+  };
   return (
     <div>
       <div
@@ -57,12 +60,9 @@ const paginate=(pageNumber)=> setCurrentPage(pageNumber)
       >
         <div className={style.container}>
           <h1> Welcome to NFTs-ECOMMERCE</h1>
-          <Search/>
+          <Search />
           <label htmlFor="">Filters/ Orders</label>
-
-          //FILTRAR POR CATEGORIAS 
-    
-                  
+          //FILTRAR POR CATEGORIAS
           {/* <select  name="categories" onChange={e=>filterByCategories(e)}>
            <option value="">Temperaments</option>
            <option value="all">All</option>      
@@ -71,64 +71,57 @@ const paginate=(pageNumber)=> setCurrentPage(pageNumber)
             cat.name.slice(1)}>{cat.name && cat.name.charAt(0).toUpperCase()+cat.name.slice(1)}</option>
           })}      
            </select> */}
-
-           //ORDENAR POR ABC
-        <select onChange={e=> filterAscDesc(e)}>
-          <option value="">Asc-Desc</option>
-          <option value="az">A-Z</option>
-          <option value="za">Z-A</option>
-        </select>
-
-        {/* <select className="selects" onChange={e=>filterByCreated(e)}>
+          //ORDENAR POR ABC
+          <select onChange={(e) => filterAscDesc(e)}>
+            <option value="">Asc-Desc</option>
+            <option value="az">A-Z</option>
+            <option value="za">Z-A</option>
+          </select>
+          {/* <select className="selects" onChange={e=>filterByCreated(e)}>
         <option value="">By created</option>
          <option value="all">All</option>
           <option value="created">Created Dogs</option>
           <option value="apiDogs">Api Dogs</option>
         </select> */}
-
-        {/* <select className="selects" onChange={e=>filterByWeight(e)}>
-        <option value="">By weight</option>
-         <option value="all">All</option>
-          <option value="max">Max</option>
-          <option value="min">Min</option>
-        </select>
-     */}
-       {/* <Link to="/dogs/add"><button class="buttonDog">Create Dog</button></Link>
+          <select onChange={(e) => filterByPrice(e)}>
+            <option value="all">Price</option>
+            <option value="max">Max</option>
+            <option value="min">Min</option>
+          </select>
+          {/* <Link to="/dogs/add"><button class="buttonDog">Create Dog</button></Link>
        <button class="buttonDog" onClick={(e)=>allDogs(e)}>All Dogs</button>
-       </div> */} 
-
-
-
-
-
-          {currentNft && currentNft.length >0 ? (
+       </div> */}
+          {currentNft && currentNft.length > 0 ? (
             currentNft.map((n) => (
               <div key={n._id}>
-                <Link  to={`nft/${n._id}`}> <h4 ><h2>{n.name}</h2></h4></Link>                
+                <Link to={`nft/${n._id}`}>
+                  {" "}
+                  <h4>
+                    <h2>{n.name}</h2>
+                  </h4>
+                </Link>
                 <img src={n.image || n.iconUrl} />
                 <p>{n.description}</p>
-                
+                <p>categorie: {n.dappName} </p>
+                <p> price: {n.price}</p>
               </div>
             ))
           ) : (
             <p>loading...</p>
           )}
-
-</div>
-     
-    </div>
-          <div>
-            <Paginated
-            
-              PerPage={PerPage}
-              stateAllNFTs={stateAllNFTs.length}
-              paged={paginate}
-            />
-            {/* {currentNft.length?.map((e, index) => {
+        </div>
+      </div>
+      <div>
+        <Paginated
+          PerPage={PerPage}
+          stateAllNFTs={stateAllNFTs.length}
+          paged={paginate}
+        />
+        {/* {currentNft.length?.map((e, index) => {
               // le pregunto si hay AllGames, (tiene el estado global con todos los juegos) y desp por cada juego le paso un card con cada prop que quiero renderizar
               return <p key={index} nft={e} />;
             })} */}
-          {/* </div> */}
+        {/* </div> */}
         {/* </div> */}
       </div>
     </div>
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,13 +1,13 @@
-
-import { GET_NTFs, SET_LOADING,  IS_AUTORIZATED } from "../actions/constants";
+import { GET_NTFs, SET_LOADING, IS_AUTORIZATED } from "../actions/constants";
 
 const initialState = {
   allNFTs: [], // state of all NFTS from API openSea
   filtered: [], // contains array for make the filters
   loading: true, //  boolean for show a image when is loading. Set first : true
-  userIsAuthenticated:[],
+  userIsAuthenticated: [],
   page: 1,
-  nftDetail:[],
+  nftDetail: [],
+  Nfts: [],
 };
 
 function rootReducer(state = initialState, action) {
@@ -19,12 +19,13 @@ function rootReducer(state = initialState, action) {
         allNFTs: action.payload,
         filtered: action.payload,
         loading: false,
+        Nfts: action.payload,
       };
-     case IS_AUTORIZATED :
-       return {
+    case IS_AUTORIZATED:
+      return {
         ...state,
-        userIsAuthenticated: action.payload
-            };
+        userIsAuthenticated: action.payload,
+      };
     case SET_LOADING:
       return {
         ...state,
@@ -36,47 +37,67 @@ function rootReducer(state = initialState, action) {
         page: action.payload,
       };
     case "GET_NFT_BY_NAME":
-      return{
+      return {
         ...state,
-        allNFTs:action.payload
-      }
+        allNFTs: action.payload,
+      };
     case "GET_NFT_BY_ID":
-      return{
+      return {
         ...state,
-        nftDetail:action.payload
-      }
-      case  'FILTER_BY_DES_ASC':{
-        const ascDescFilter = action.payload==="za"? state.allNFTs.sort((a, b) => {
-          // cat.name.charAt(0).toUpperCase()
-          if (a.name !==null && b.name !==null){
-            if(a.name?.charAt(0).toLowerCase() < b.name?.charAt(0).toLowerCase()) {
-                return 1
-            } else {
-                return - 1
-            }
-          }}):
-        state.allNFTs.sort((a, b) => {
-          if (a.name !==null && b.name !==null){
-            if(a.name?.charAt(0).toLowerCase() > b.name?.charAt(0).toLowerCase()) {
-                    return 1
+        nftDetail: action.payload,
+      };
+    case "FILTER_BY_DES_ASC":
+      const ascDescFilter =
+        action.payload === "za"
+          ? state.allNFTs.sort((a, b) => {
+              // cat.name.charAt(0).toUpperCase()
+              if (a.name !== null && a.name > b.name !== null) {
+                if (
+                  a.name?.charAt(0).toLowerCase() <
+                  b.name?.charAt(0).toLowerCase()
+                ) {
+                  return 1;
                 } else {
-                    return - 1
+                  return -1;
                 }
-            }}) 
+              }
+            })
+          : state.allNFTs.sort((a, b) => {
+              if (a.name !== null && b.name !== null) {
+                if (
+                  a.name?.charAt(0).toLowerCase() >
+                  b.name?.charAt(0).toLowerCase()
+                ) {
+                  return 1;
+                } else {
+                  return -1;
+                }
+              }
+            });
 
-            console.log(ascDescFilter,"asi queda")
-            return{
-                ...state,
-                allNFTs: [...ascDescFilter]
-            }
-          
-    }
-    
-    
+      console.log(ascDescFilter, "asi queda");
+      return {
+        ...state,
+        allNFTs: [...ascDescFilter],
+      };
+    case "SORT_PRICE":
+      const priceFilter =
+        action.payload === "max"
+          ? [...state.Nfts].sort(
+              (b, a) => parseInt(a.price) - parseInt(b.price)
+            )
+          : [...state.Nfts].sort(
+              (b, a) => parseInt(b.price) - parseInt(a.price)
+            );
+      console.log(priceFilter, priceFilter.length);
+      return {
+        ...state,
+        allNFTs: priceFilter,
+      };
+
     default:
       return state;
   }
-
 }
 
 export default rootReducer;
